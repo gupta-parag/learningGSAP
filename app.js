45d1ac46a517d1eebfabbfea4e40e6c40217afde
gupta-parag <parag.gupta@protonmail.com>
2023-10-11 21:44:47 -0400

    Added first lesson on GSAP library

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("Javascript file linked");
+
+// gsap.from("#box", {
+//   x: 0,
+//   y: -100,
+//   duration: 1,
+//   delay: 1,
+//   // backgroundColor: "black",
+//   // rotate: 360,
+// });
+
+gsap.from("li", {
+  x: 0,
+  y: -100,
+  duration: 1,
+  delay: 0.5,
+  stagger: true,
+  // backgroundColor: "black",
+  // rotate: 360,
+});
+
+// gsap.to("#box", {
+//   x: 1600,
+//   y: 0,
+//   duration: 2,
+//   delay: 3,
+//   backgroundColor: "crimson",
+//   rotate: 720,
+// });
+
+// gsap.to("#box", {
+//   x: 0,
+//   y: 0,
+//   duration: 2,
+//   delay: 5,
+//   backgroundColor: "blue",
+//   rotate: 1080,
+// });
